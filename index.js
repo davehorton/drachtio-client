aada663fd222582aa36661ce66b2c687c7a2db67
Dave Horton <daveh@beachdognet.com>
2015-02-13 13:26:51 -0500

    further changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Agent = require('./lib/agent') ;
 var Request = require('./lib/request') ;
 var Response = require('./lib/response') ;
+var onSend = require('./lib/onSend') ;
 
 exports = module.exports = {
-	Agent: Agent
-	,Request: Request
-	,Response: Response
-}
+	Agent: Agent,
+	Request: Request,
+	Response: Response,
+	onSend: onSend
+} ;
